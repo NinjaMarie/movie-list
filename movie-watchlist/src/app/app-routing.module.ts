49623ba63f19713bf00f2075b2037898218ec5ef
Ninjamarie <ninamarie.mamongay9@gmail.com>
2023-12-04 23:47:37 +0800

    edit: UI Changes

--- movie-watchlist/src/app/app-routing.module.ts
+++ movie-watchlist/src/app/app-routing.module.ts
@@ -1,17 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MovieCardComponent } from './cards/movie-card/movie-card.component';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
-import { DetailCardComponent } from './cards/detail-card/detail-card.component';
 import { MovieDetailPageComponent } from './pages/movie-detail-page/movie-detail-page.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
-import { WatchListCardComponent } from './cards/watch-list-card/watch-list-card.component';
 import { WatchListPageComponent } from './pages/watch-list-page/watch-list-page.component';
 
 const routes: Routes = [
   { path: '', component: LandingPageComponent },
   { path: 'movie-detail/:id', component: MovieDetailPageComponent },
   { path: 'movie-detail/:id/watch-list', component: WatchListPageComponent },
+  { path: 'watch-list', component: WatchListPageComponent },
   { path: '**', component: PageNotFoundComponent },
 ];
 
